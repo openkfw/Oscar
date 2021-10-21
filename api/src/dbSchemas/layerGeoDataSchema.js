3e8e13ee752dfe26253dd94d72544a9332053671
Sarka Rezacova <sarka.rezacova@accenture.com>
2021-10-21 22:16:27 +0200

    #69-change geodata schema in api

--- api/src/dbSchemas/layerGeoDataSchema.js
+++ api/src/dbSchemas/layerGeoDataSchema.js
@@ -1,10 +1,26 @@
 const mongoose = require('mongoose');
 
+const featureIdSchema = new mongoose.Schema(
+  {
+    property: String,
+    values: [{ type: String }],
+  },
+  { _id: false },
+);
+
 const layerGeoDataSchema = new mongoose.Schema({
   referenceId: { type: String, index: { unique: true } },
   name: String,
   geoDataUrl: String,
   updateDate: String,
+  format: String,
+  featureId: [featureIdSchema],
+  attributeIds: [{ type: String }],
+  geometryDataTypes: {
+    type: String,
+    enum: ['points', 'regions', 'geometry'],
+  },
+  metadata: Object,
 });
 
 module.exports = mongoose.model('LayerGeoData', layerGeoDataSchema, 'layerGeoData');
