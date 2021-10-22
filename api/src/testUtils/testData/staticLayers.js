2f37110f82dd565d87d93d75f0847fa4ecd8c33b
Sarka Rezacova <sarka.rezacova@accenture.com>
2021-10-22 14:36:24 +0200

    #69-test if geometadata returns correctly

--- api/src/testUtils/testData/staticLayers.js
+++ api/src/testUtils/testData/staticLayers.js
@@ -3,6 +3,19 @@ const layerGeoDataInDb = {
   referenceId: 'sampleGeoJSON',
   geoDataUrl: '/api/uploads/geojsons/1600424721066.geojson',
   updateDate: '1600424721492',
+  format: 'geojson',
+  featureIds: [
+    { property: 'name', values: ['Name of the Feature1', 'Name of the Feature2'] },
+    { property: 'fullname', values: ['Fullname of the Feature1', 'Fullname of the Feature2'] },
+  ],
+  attributeIds: ['id', 'amenity'],
+  geometryDataTypes: 'points',
+  metadata: {
+    description: 'Sample data created just for this purpose. Do not represent the reality in any time.',
+    updateFrequency: 'never',
+    unit: 'n/a',
+    dataRetrievalDescription: 'Data was randomly created.',
+  },
 };
 
 const mapLayersInDb = [
--- api/src/tests/staticLayers.js
+++ api/src/tests/staticLayers.js
@@ -20,7 +20,7 @@ describe('GET /api/staticLayers', () => {
     expect(res.status).toEqual(200);
     expect(res.body).toHaveLength(0);
   });
-  it('should return all layers in db sorted by title', async () => {
+  it('should return all layers in db sorted by title with geoDataUrl and metadata from layerGeoData collection', async () => {
     await LayerGeoData.create(layerGeoDataInDb);
     // saved in reverse order to check the sorting
     await MapLayer.create(mapLayersInDb[1]);
@@ -32,8 +32,9 @@ describe('GET /api/staticLayers', () => {
     expect(res.body[0].geoReferenceId).toEqual(layerGeoDataInDb.referenceId);
     expect(res.body[0].referenceId).toEqual(mapLayersInDb[0].referenceId);
     expect(res.body[0].geoDataUrl).toEqual(layerGeoDataInDb.geoDataUrl);
+    expect(res.body[0].metadata).toEqual(layerGeoDataInDb.metadata);
   });
-  it('should return layer of type group with correct geoJSON links in sublayers', async () => {
+  it('should return layer of type group with correct geoJSON links and metadata in sublayers', async () => {
     await LayerGeoData.create(layerGeoDataInDb);
     await GroupMapLayer.create(mapLayersInDb[2]);
 
@@ -43,6 +44,7 @@ describe('GET /api/staticLayers', () => {
     expect(res.body[0].referenceId).toEqual(mapLayersInDb[2].referenceId);
     res.body[0].layers.forEach((layer) => {
       expect(layer.geoDataUrl).toEqual(layerGeoDataInDb.geoDataUrl);
+      expect(layer.metadata).toEqual(layerGeoDataInDb.metadata);
     });
   });
 });
