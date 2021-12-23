21c75e9369ca793d5fc6e3ef3650947cb63cfd66
Daniela Barborova <daniela.barborova@accenture.com>
2021-12-23 12:00:13 +0100

    in api attributes route, filter by featureId also when latestValues flag

--- api/src/routes/attributes.js
+++ api/src/routes/attributes.js
@@ -23,7 +23,7 @@ router.get(
     let items = [];
 
     if (latestValues) {
-      items = await getLatestAttributes(attributeId, attributeIdCategory).catch((e) =>
+      items = await getLatestAttributes(attributeId, attributeIdCategory, featureIds).catch((e) =>
         logger.error(`Error: ${e.message}`),
       );
     } else {
