67f9f7bd4bd6292aa04b8c3995661c0e28c59b24
martin.jurcoglina <martin.jurcoglina@accenture.com>
2022-08-03 17:39:38 +0200

    #116-test fix

--- api/src/config/swagger.ts
+++ api/src/config/swagger.ts
@@ -1,6 +1,6 @@
 import swaggerValidation from 'openapi-validator-middleware';
 import config from './config';
 
-swaggerValidation.init(config.openApiSchemaFile);
+swaggerValidation.init(config.openApiSchemaFile || 'src/openapi/apiSchema.yml');
 
 export default swaggerValidation;
